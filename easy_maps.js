fbbed6780fd667bf60f17d0171e980f97cdba13d
Wouter J <waldio.webdesign@gmail.com>
2011-08-18 17:22:48 +0200

    Marker class added, NOT WORKING

--- easy_maps.js
+++ easy_maps.js
@@ -12,20 +12,40 @@
 // Let's create a workaround
 (function(window,document,undefined) {
 
-	var maps;
+	/*
+	 * @name	: extend
+	 * @type	: Function
+	 * @visibility	: private
+	 * @use		: For extending 2 arrays
+	 * @parameters	: OBJECT arrayOne
+	 * 		  OBJECT arrayTwo
+	 */
+	var extend = function( aOne, aTwo ) {
+		if( arguments.length != 2 ) 
+			return false;
+
+		for( i in aTwo ) {
+			if( !aOne[i] ) {
+				aOne[i] = aTwo[i];
+			}
+		}
+
+		return aOne;
+	};
 
 	/*
 	 * @name	: Position
 	 * @type	: Object
+	 * @visibility	: public
 	 * @use		: For creating a coördinate
 	 * @parameters	: OBJECT ll  ||  STRING place
 	 */
 	var position = function() {
 		var args = arguments,
 		    len = args.length;
-		if( len < 1 ) {
+
+		if( len < 1 ) 
 			return false;
-		}
 		
 		if( len == 1 && typeof args[0] == 'string' ) {
 			return new google.maps.LatLng(args[0]);
@@ -40,15 +60,15 @@
 	/*
 	 * @name	: Map
 	 * @type	: Object
+	 * @visibility	: public
 	 * @use		: For creating a new google map
 	 * @parameters	: DOM elem
 	 * 		  OBJECT options
 	 * @TODO	: Elem dom check
 	 */
 	var map = function( elem, options ) {
-		if( arguments.length < 1 ) {
+		if( arguments.length < 1 ) 
 			return false;
-		}
 
 		var defaults = {
 			zoom : 1,
@@ -56,28 +76,53 @@
 			mapTypeId : google.maps.MapTypeId.ROADMAP
 		};
 		if( options !== undefined ) {
-			for( i in defaults ) {
-				if( !options[i] ) {
-					options[i] = defaults[i];
-				}
-			}
+			options = extend( options, defaults );
 		}
 		else {
 			options = defaults;
 		}
 
-		maps = new google.maps.Map(elem, options);
+		this.maps = new google.maps.Map(elem, options);
+
+		return this;
+	};
+
+	/*
+	 * @name	: map.addMarker
+	 * @type	: Function
+	 * @visibility	: public
+	 * @use		: To add a marker to a map
+	 * @parameters	: MARKER marker
+	 */
+	map.addMarker = function( marker ) {
+		marker.setMap(this.map);
 	};
 
 	/*
 	 * @name	: Marker
 	 * @type	: Object
+	 * @visibility	: public
 	 * @use		: For creating a marker
 	 * @parameters	: OBJECT options
 	 */
+	var marker = function( options ) {
+		if( arguments.length != 1 || typeof options !== 'object' ) 
+			return false;
+
+		var defaults = {
+			position : position(0,0)
+		};
+
+		var options = extend(options, defaults);
+
+		this.marker = new google.maps.Marker(options);
+
+		return this.marker;
+	};
 
 	// Every object should work in the normal workaround
-	     window.Map = map;
 	window.Position = position;
+	     window.Map = map;
+	  window.Marker	= marker;
 
 })(window, document);
